53cdabcda1d8779ac47a0ef0afa1ac2306bcd688
tetete2016 <tetetelego2013@gmail.com>
2016-08-15 15:18:44 +0900

    added log

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
-
+console.log("app is running");
 var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
